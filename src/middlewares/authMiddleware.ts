9c83524fd1cafc1c1edd5c45b5dc2e5dd36b1499
Abdul Rehman Danish <asusrogg73sw2019@gmail.com>
2026-03-13 03:29:40 +0500

    authMiddleware update for cookie

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -4,47 +4,41 @@ import asyncHandler from './asyncHandler';
 import User from '../models/userModel';
 
 interface AuthRequest extends Request {
-    user?: any;
+  user?: any;
 }
 
 export const protect = asyncHandler(
   async (req: AuthRequest, res: Response, next: NextFunction) => {
 
-    let token;
-
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith('Bearer')
-    ) {
-      try {
-        token = req.headers.authorization.split(' ')[1];
-
-        if (!process.env.JWT_SECRET) {
-          throw new Error("JWT_SECRET not defined");
-        }
-
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
-
-        const user = await User.findById(decoded.id).select('-password');
-
-        if (!user) {
-          res.status(401);
-          throw new Error("Not authorized, user not found");
-        }
-
-        req.user = user;
-
-        next();
-
-      } catch (error) {
-        res.status(401);
-        throw new Error('Not authorized, token failed');
-      }
-    }
+    const token = req.cookies?.token; // 👈 cookie se token
 
     if (!token) {
       res.status(401);
       throw new Error('Not authorized, no token');
+    }
+
+    try {
+
+      if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET not defined");
+      }
+
+      const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
+
+      const user = await User.findById(decoded.id).select('-password');
+
+      if (!user) {
+        res.status(401);
+        throw new Error("Not authorized, user not found");
+      }
+
+      req.user = user;
+
+      next();
+
+    } catch (error) {
+      res.status(401);
+      throw new Error('Not authorized, token failed');
     }
 });
 
